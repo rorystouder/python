97bb7a272061656ba342bd28f96a6ba2352fd477
rorystouder <rory.stouder@gmail.com>
2020-12-07 09:47:04 -0500

    File IO 2

--- series.py
+++ series.py
@@ -1,15 +1,10 @@
+"""Read and print an integer series"""
 import sys
 
 def read_series(filename):
-    try:
-        f = open(filename, mode='rt', encoding='utf-8')
-        return [int(line.strip()) for line in f]
-    finally:
-        f.close()
+        with open(filename, mode='rt', encoding='utf-8') as f:
+            return [int(line.strip()) for line in f]
 
 def main(filename):
     series = read_series(filename)
     print(series)
-
-if __name__ == '__main__':
-    read_series(filename=sys.argv[1])
